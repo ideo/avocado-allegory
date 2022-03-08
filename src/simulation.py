renamed a variable to make it clearer. Fixed a bug in calculation of avg, sum, median

src/simulation.py
@@ -6,19 +6,18 @@ from .townspeople import Townsperson
 class Simulation:
     def __init__(
             self, guac_df, num_townspeople, st_dev, 
-            limit=20, perc_fra=0.0, perc_pepe=0.0
+            num_guac_per_person=20, perc_fra=0.0, perc_pepe=0.0, method="sum"
         ):
         self.guac_df = guac_df
         self.num_townspeople = num_townspeople
         self.st_dev = st_dev
-        self.limit = limit
+        self.num_guac_per_person = num_guac_per_person
         self.perc_fra = perc_fra
         self.perc_pepe = perc_pepe
         self.results_df = None
         self.objective_winner = guac_df[["Objective Ratings"]].idxmax()[0]
-        self.winner = None
         self.fra_joe = 'joe'
-        # self.method = method.lower()
+        self.method = method.lower()
 
 
     def simulate(self):
@@ -26,26 +25,32 @@ class Simulation:
         num_fras = round(self.num_townspeople * self.perc_fra)
         num_reasonable = self.num_townspeople - num_pepes - num_fras
 
+        # This can be more DRY
         name = 0
         self.results_df = pd.DataFrame(index=self.guac_df.index)
+        for _ in range(num_reasonable):
+            person = Townsperson(name=name, st_dev=self.st_dev)
+            self.results_df[person.name] = person.taste_and_vote(self.guac_df)
+            name += 1
 
-        person_types = [num_reasonable, num_pepes, num_fras]
-        mean_offsets = [0, 3, -3]
+        for _ in range(num_pepes):
+            offset = 3
+            person = Townsperson(name=name, st_dev=self.st_dev, mean_offset=offset)
+            self.results_df[person.name] = person.taste_and_vote(self.guac_df)
+            name += 1
 
-        for num_people, offset in zip(person_types, mean_offsets):
-            for _ in range(num_people):
-                person = Townsperson(name=name, st_dev=self.st_dev, limit=self.limit, mean_offset=offset)
-                self.results_df[person.name] = person.taste_and_vote(self.guac_df)
-                name += 1
+        for _ in range(num_fras):
+            offset = -3
+            person = Townsperson(name=name, st_dev=self.st_dev, mean_offset=offset)
+            self.results_df[person.name] = person.taste_and_vote(self.guac_df)
+            name += 1
 
-        self.results_df["sum"] = self.results_df.sum(axis=1)
-        self.winner = self.results_df[["sum"]].idxmax()[0]
+        cols_to_use = self.results_df.columns
+        sum_of_votes = self.results_df[cols_to_use].sum(axis=1)
+        avg_of_votes = self.results_df[cols_to_use].mean(axis=1)
+        med_of_votes = self.results_df[cols_to_use].median(axis=1)
+        self.results_df["sum"] = sum_of_votes
+        self.results_df["avg"] = avg_of_votes
+        self.results_df["med"] = med_of_votes
 
-        # sum_of_votes = self.results_df.sum(axis=1)
-        # avg_of_votes = self.results_df.mean(axis=1)
-        # med_of_votes = self.results_df.median(axis=1)
-        # self.results_df["sum"] = sum_of_votes
-        # self.results_df["avg"] = avg_of_votes
-        # self.results_df["med"] = med_of_votes
-
-        
+        self.winner = self.results_df[[self.method]].idxmax()[0]